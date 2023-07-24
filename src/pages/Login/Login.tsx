2adb9861466aecb24aaf38abed43c3efab1f8f56
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2023-07-24 21:01:59 +0600

    data getting done from login& signUp form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,44 +1,57 @@
 import { Link } from "react-router-dom";
 const Login = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleLogin = (e: any) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log("login info:", email, password);
+  };
+
   return (
     <div className="flex justify-center items-center">
       <div className="w-[350px] h-[400px] bg-slate-300 p-10 my-10">
         <h1 className="text-center text-2xl font-bold mb-4">Login !</h1>
-        <input
-          className="w-full border px-2 py-1 rounded-xl mb-5"
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Your Email"
-          required
-        />
-        <br />
-        <input
-          className="w-full border px-2 py-1 rounded-xl mb-5"
-          type="password"
-          name="password"
-          id="pass"
-          placeholder="Your Password"
-          required
-        />
-        <p className="mb-3">
-          Are you new here?{" "}
-          <Link to="/signUp">
-            <span className="text-primary font-bold">SignUp</span>
-          </Link>
-        </p>
-        <input
-          className="w-full bg-primary text-white px-2 py-1 rounded-lg"
-          type="button"
-          value="Login"
-        />
-        <p className="text-xl text-center my-5">OR</p>
+        <form onSubmit={handleLogin}>
+          <input
+            className="w-full border px-2 py-1 rounded-xl mb-5"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Your Email"
+            required
+          />
+          <br />
+          <input
+            className="w-full border px-2 py-1 rounded-xl mb-5"
+            type="password"
+            name="password"
+            id="pass"
+            placeholder="Your Password"
+            required
+          />
+          <p className="mb-3">
+            Are you new here?{" "}
+            <Link to="/signUp">
+              <span className="text-primary font-bold">SignUp</span>
+            </Link>
+          </p>
+          <button
+            type="submit"
+            className="w-full bg-primary text-white px-2 py-1 rounded-lg"
+          >
+            Login
+          </button>
+          <p className="text-xl text-center my-5">OR</p>
 
-        <input
-          className="w-full bg-sec_primary px-2 py-1 rounded-lg"
-          type="button"
-          value="Login with Google"
-        />
+          <input
+            className="w-full bg-sec_primary px-2 py-1 rounded-lg"
+            type="button"
+            value="Login with Google"
+          />
+        </form>
       </div>
     </div>
   );
--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,54 +1,72 @@
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleSignUp = (e: any) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log("Sign Up info", email, password);
+  };
+
   return (
     <div className="flex justify-center items-center">
       <div className="w-[350px] h-[430px] bg-slate-300 p-10 my-10">
         <h1 className="text-center text-2xl font-bold mb-4">Sign Up !</h1>
-        <input
-          className="w-full border px-2 py-1 rounded-xl mb-5"
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Your Name"
-          required
-        />
-        <br />
-        <input
-          className="w-full border px-2 py-1 rounded-xl mb-5"
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Your Email"
-          required
-        />
-        <br />
-        <input
-          className="w-full border px-2 py-1 rounded-xl mb-5"
-          type="password"
-          name="password"
-          id="pass"
-          placeholder="Your Password"
-          required
-        />
-        <p className="mb-3">
-          Already have an account?{" "}
-          <Link to="/login">
-            <span className="text-primary font-bold">Login</span>
-          </Link>
-        </p>
-        <input
-          className="w-full bg-primary text-white px-2 py-1 rounded-lg"
-          type="button"
-          value="Sign Up"
-        />
-        <p className="text-xl text-center my-2">OR</p>
+        <form onSubmit={handleSignUp}>
+          <input
+            className="w-full border px-2 py-1 rounded-xl mb-5"
+            type="text"
+            name="name"
+            id="name"
+            placeholder="Your Name"
+            required
+          />
+          <br />
+          <input
+            className="w-full border px-2 py-1 rounded-xl mb-5"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Your Email"
+            required
+          />
+          <br />
+          <input
+            className="w-full border px-2 py-1 rounded-xl mb-5"
+            type="password"
+            name="password"
+            id="pass"
+            placeholder="Your Password"
+            required
+          />
+          <p className="mb-3">
+            Already have an account?{" "}
+            <Link to="/login">
+              <span className="text-primary font-bold">Login</span>
+            </Link>
+          </p>
+          <button
+            type="submit"
+            className="w-full bg-primary text-white px-2 py-1 rounded-lg"
+          >
+            Sign Up
+          </button>
+          {/* <input
+            className="w-full bg-primary text-white px-2 py-1 rounded-lg"
+            type="button"
+            value="Sign Up"
+          /> */}
+          <p className="text-xl text-center my-2">OR</p>
 
-        <input
-          className="w-full bg-sec_primary px-2 py-1 rounded-lg"
-          type="button"
-          value="Sign up with Google"
-        />
+          <input
+            className="w-full bg-sec_primary px-2 py-1 rounded-lg"
+            type="button"
+            value="Sign up with Google"
+          />
+        </form>
       </div>
     </div>
   );
